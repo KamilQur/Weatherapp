5c4d5f65dd31fa3f76b3bdf8e4f72a6e915e951c
Blog app <mkq001@gmail.com>
2017-05-26 00:59:31 +0500

    Search bar working

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,24 +5,20 @@ var About = (props) => {
     <div>
       <h1 className="text-center">About</h1>
       <p>
-        This is a weather application build on React. I have built this
-        for The Complete React Web App Developer Course.
+        Welcome to Weatherapp Where you Search 
       </p>
+
       <p>
-        Here are some of the tools I used:
+      Weather of any city in the World. 
       </p>
-      <ul>
-        <li>
-          <a href="https://facebook.github.io/react">React</a> - This was the
-            JavaScript framework used.
-        </li>
-        <li>
-          <a href="http://openweathermap.org">Open Weather Map</a> - I used
-            Open Weather Map to search for weather data by city name.
-        </li>
-      </ul>
+      
+      <p>
+        The Temprature will be shown in ℃ in the app 
+      </p>
+
     </div>
   )
 };
+
 
 module.exports = About;
